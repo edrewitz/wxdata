cfd7f6175e140971a839b0c3550b7a4e81a69d1e
Eric J. Drewitz <101157849+edrewitz@users.noreply.github.com>
2025-07-25 22:44:13 -0700

    Update scanner.py

--- wxdata/scanner.py
+++ wxdata/scanner.py
@@ -1,184 +1,39 @@
-"""
-This file has the scanner function which scans the data server for the latest FULL dataset and returns the download url
-
-(C) Eric J. Drewitz 
-"""
-
-# Imports the needed packages
-import requests
+import numpy as np
 import os
-import time
 
-# Exception handling for Python >= 3.13 and Python < 3.13
-try:
-    from datetime import datetime, timedelta, UTC
-except Exception as e:
-    from datetime import datetime, timedelta
+from cartopy.util import add_cyclic_point
 
-# Gets current time in UTC
-try:
-    now = datetime.now(UTC)
-except Exception as e:
-    now = datetime.utcnow()
 
-# Gets local time
-local = datetime.now()
-
-# Gets yesterday's date
-yd = now - timedelta(days=1)
-
-def file_extension(fname):
+def clear_idx_files(step=None, model=None, cat=None, paths=None, ens=False):
 
     """
-    Scans for file extension.
+    This function clears all the .IDX files in a folder.
     """
-
-    if fname[-4] == 'f':
-        ftype = False
+    
+    if ens == False:
+        try:
+            for item in os.listdir(f"{model}/{cat}/{step}"):
+                if item.endswith(".idx"):
+                    os.remove(f"{model}/{cat}/{step}/{item}")
+        except Exception as e:
+            pass
+            
     else:
-        ftype = True
-
-    return ftype
-
-def file_fhour_checker(model, fname, max_fcst_hour):
-
-    """
-    This function returns the string-index of the model run times in a file
-
-    1) model (String) - The forecast model
-
-    Optional Arguments: None
-
-    Returns
-    -------
-
-    The index values of the run times in the file. 
-    """
-    
-    hr = int(f"{fname[-9]}{fname[-8]}{fname[-7]}")
-
-    if hr == max_fcst_hour:
-        download = False
-    else:
-        download = True
-
-    return download
-
-
-def forecast_hour(model):
-
-    """
-    This function returns the latest forecast hour for each model
-    """
-
-    hour = {
-        'GEFS0P25':240,
-        'GEFS0P50':384,
-        'GEFS0P50 SECONDARY PARAMETERS':384        
-    }
-    
-    return hour[model]
-
-def ensemble_members(model):
-
-    """
-    This function returns the number of ensemble members for an ensemble
-
-    Required Arguments:
-
-    1) model (String)
-
-    Returns
-    -------
-    The number of ensemble members for a particular ensemble
-    """
-
-    members = {
-        'GEFS0P25':30,
-        'GEFS0P50':30,
-        'GEFS0P50 SECONDARY PARAMETERS':30
+        try:
+            for p in paths:
+                for item in os.listdir(f"{p}"):
+                    if item.endswith(".idx"):
+                        os.remove(f"{paths[p]}/{item}")
+        except Exception as e:
+            pass
         
-    }
 
-    return members[model]
-        
-def url_index(model):
-
-    """
-    This function returns the string-index of the model run times in a file
-
-    1) model (String) - The forecast model
-
-    Optional Arguments: None
-
-    Returns
-    -------
-
-    The index values of the run times in the file. 
-    """
+def ens_folders(model, cat, step, ens_members):
     
-    times = {
-        'GEFS0P25':[-19, -18],
-        'GEFS0P50':[-18, -17],
-        'GEFS0P50 SECONDARY PARAMETERS':[-18, -17]
-    }
-
-    return times[model][0], times[model][1]
-
-
-def index(model):
-
     """
-    This function returns the string-index of the model run times in a file
-
-    1) model (String) - The forecast model
-
-    Optional Arguments: None
-
-    Returns
-    -------
-
-    The index values of the run times in the file. 
+    This function builds the directories for ensemble members
     """
-    
-    times = {
-        'GEFS0P25':[7, 8],
-        'GEFS0P50':[7, 8],
-        'GEFS0P50 SECONDARY PARAMETERS':[7, 8]
-    }
 
-    return times[model][0], times[model][1]
-
-def file_scanner(model, cat, url, url_run, step, ens_members=False):
-
-    """
-    This function scans the directory to make sure: 
-    
-    1) The directory branch exists. 
-    2) Builds the directory branch if it does not exist
-    3) Makes sure the files are up to date
-
-    Required Arguments: 
-
-    1) model (String) - The model the user wants. 
-
-    2) cat (String) - The category of data the user wants (i.e. ensmean vs. enscontrol). 
-
-    3) url (String) - The URL returned from the url_scanner function. 
-
-    4) url_run (Integer) - The model run time in the URL returned from the url_scanner function. 
-
-    Returns
-    -------
-
-    1) A boolean value of True or False for download.
-    """    
-    model = model.upper()
-    cat = cat.upper()
-
-    aa, bb = index(model)
-    hour = forecast_hour(model)
-    
     if os.path.exists(f"{model}"):
         pass
     else:
@@ -194,206 +49,136 @@ def file_scanner(model, cat, url, url_run, step, ens_members=False):
     else:
         os.mkdir(f"{model}/{cat}/{step}")
 
-    exists = False
-
-    if ens_members == False:
-        try:
-            fnames = []
-            for file in os.listdir(f"{model}/{cat}/{step}"):
-                fname = os.path.basename(f"{model}/{cat}/{step}/{file}")
-                fnames.append(fname)
-            fname = fnames[-1]
-            ftype = file_extension(fname)
-            exists = True
-        except Exception as e:
-            download = True
-    
-        if exists == False:
-            download = True
-    
+    paths = []
+    for i in range(1, ens_members + 1, 1):
+        if os.path.exists(f"{model}/{cat}/{step}/{i}"):
+            pass
         else:
-            file_run = int(f"{fname[aa]}{fname[bb]}")
-            if file_run == url_run:
-                modification_timestamp = os.path.getmtime(f"{model}/{cat}/{step}/{fname}")
-                readable_time = time.ctime(modification_timestamp)
-                update_day = int(f"{readable_time[8]}{readable_time[9]}")
-                update_hour = int(f"{readable_time[11]}{readable_time[12]}") 
-                if update_day != local.day:
-                    download = True
-                else:
-                    tdiff = local - timedelta(hours=6)
-                    if update_hour < tdiff.hour:
-                        download = True
-                    else:
-                        if ftype == False:
-                            download = True
-                        else:
-                            max_fcst_hour = forecast_hour(model)
-                            download = file_fhour_checker(model, fname, max_fcst_hour)
-                
-            else:
-                download = True
+            os.mkdir(f"{model}/{cat}/{step}/{i}")
 
-    else:
-        members = ensemble_members(f"{model}")
-        try:
-            fnames = []
-            for file in os.listdir(f"{model}/{cat}/{step}/{members}"):
-                fname = os.path.basename(f"{model}/{cat}/{step}/{members}/{file}")
-                fnames.append(fname)
-            fname = fnames[-1]
-            ftype = file_extension(fname)
-            exists = True
-        except Exception as e:
-            download = True
+        path = f"{model}/{cat}/{step}/{i}"
 
-        if exists == False:
-            download = True
-    
-        else:
-            file_run = int(f"{fname[aa]}{fname[bb]}")
-            if file_run == url_run:
-                modification_timestamp = os.path.getmtime(f"{model}/{cat}/{step}/{members}/{fname}")
-                readable_time = time.ctime(modification_timestamp)
-                update_day = int(f"{readable_time[8]}{readable_time[9]}")
-                update_hour = int(f"{readable_time[11]}{readable_time[12]}") 
-                if update_day != local.day:
-                    download = True
-                else:
-                    tdiff = local - timedelta(hours=6)
-                    if update_hour < tdiff.hour:
-                        download = True
-                    else:
-                        if ftype == False:
-                            download = True
-                        else:
-                            max_fcst_hour = forecast_hour(model)
-                            download = file_fhour_checker(model, fname, max_fcst_hour)
-                
-            else:
-                download = True
-        
-    return download     
-    
+        paths.append(path)
 
-def url_scanner(model, cat, proxies):
+    return paths
 
+def shift_longitude(ds, lon_name='longitude'):
     """
-    This function scans a webpage for the file with the latest forecast model run. 
-    If the page has complete data, the download link will be returned. 
-    If the page is incomplete, the scanner will check for the previous run data. 
-    This scanner is used so the user downloads the latest FULL dataset. 
+    Shifts longitude values to ensure continuity across the Prime Meridian.
 
-    Required Arguments: 
+    Required Arguments:
 
-    1) model (String) - The model the user wants. 
+    1) ds (xarray.dataarray) - The dataset of the model data.
 
-    2) cat (String) - The category of data the user wants (i.e. ensmean vs. enscontrol). 
+    Optional Arguments:
 
-    3) proxies (dict or None) - If the user is using a proxy server, the user must change the following:
-
-    proxies=None ---> proxies={'http':'http://url',
-                            'https':'https://url'
-                        }
+    1) lon_name (String) - Default = longitude. The abbreviation for the longitude key.
 
     Returns
     -------
 
-    1) The download link.
-    2) The time of the latest model run. 
+    An xarray.dataarray with longitude coordinates ranging from -180 to 180
     """
-    model = model.upper()
-    cat = cat.upper()
+    lon = ds[lon_name].values
+    lon_shifted = (lon + 180) % 360 - 180
+    ds = ds.assign_coords({lon_name: lon_shifted})
+    ds = ds.sortby(lon_name)
+    return ds
 
-    aa, bb = url_index(model)
-    
-    if model == 'GEFS0P25' or model == 'GEFS0P50' or model == 'GEFS0P50 SECONDARY PARAMETERS':
-        if model == 'GEFS0P25':
-            a = 's'
-            b = '25'
-            c = '25'
-            
-        if model == 'GEFS0P50':
-            a = 'a'
-            b = '5'
-            c = '50'
+def lon_bounds(western_bound, eastern_bound):
+    """
+    This function calculates the western bound with 360 being at the Prime Meridian
 
-        if model == 'GEFS0P50 SECONDARY PARAMETERS':
-            a = 'b'
-            b = '5'
-            c = '50'
+    Required Arguments:
 
-        today_00z = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{now.strftime('%Y%m%d')}/00/atmos/pgrb2{a}p{b}/"
-        today_06z = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{now.strftime('%Y%m%d')}/06/atmos/pgrb2{a}p{b}/"
-        today_12z = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{now.strftime('%Y%m%d')}/12/atmos/pgrb2{a}p{b}/"
-        today_18z = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{now.strftime('%Y%m%d')}/18/atmos/pgrb2{a}p{b}/"
-        
-        yday_00z = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{yd.strftime('%Y%m%d')}/00/atmos/pgrb2{a}p{b}/"
-        yday_06z = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{yd.strftime('%Y%m%d')}/06/atmos/pgrb2{a}p{b}/"
-        yday_12z = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{yd.strftime('%Y%m%d')}/12/atmos/pgrb2{a}p{b}/"
-        yday_18z = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{yd.strftime('%Y%m%d')}/18/atmos/pgrb2{a}p{b}/"
-    
-        if cat == 'MEAN':
-            if model == 'GEFS0P50 SECONDARY PARAMETERS':
-                f_00z = f"gec00.t00z.pgrb2{a}.0p{c}.f240"
-                f_06z = f"gec00.t06z.pgrb2{a}.0p{c}.f240"
-                f_12z = f"gec00.t12z.pgrb2{a}.0p{c}.f240"
-                f_18z = f"gec00.t18z.pgrb2{a}.0p{c}.f240"                 
-            else:
-                f_00z = f"geavg.t00z.pgrb2{a}.0p{c}.f240"
-                f_06z = f"geavg.t06z.pgrb2{a}.0p{c}.f240"
-                f_12z = f"geavg.t12z.pgrb2{a}.0p{c}.f240"
-                f_18z = f"geavg.t18z.pgrb2{a}.0p{c}.f240"
-        if cat == 'CONTROL':
-            f_00z = f"gec00.t00z.pgrb2{a}.0p{c}.f240"
-            f_06z = f"gec00.t06z.pgrb2{a}.0p{c}.f240"
-            f_12z = f"gec00.t12z.pgrb2{a}.0p{c}.f240"
-            f_18z = f"gec00.t18z.pgrb2{a}.0p{c}.f240"  
-        if cat == 'ALL MEMBERS':
-            f_00z = f"gep30.t00z.pgrb2{a}.0p{c}.f240"
-            f_06z = f"gep30.t06z.pgrb2{a}.0p{c}.f240"
-            f_12z = f"gep30.t12z.pgrb2{a}.0p{c}.f240"
-            f_18z = f"gep30.t18z.pgrb2{a}.0p{c}.f240"         
-    
-        if proxies == None:
-            t_18z = requests.get(f"{today_18z}/{f_18z}", stream=True)
-            t_12z = requests.get(f"{today_12z}/{f_12z}", stream=True)
-            t_06z = requests.get(f"{today_06z}/{f_06z}", stream=True)
-            t_00z = requests.get(f"{today_00z}/{f_00z}", stream=True)
-    
-            y_18z = requests.get(f"{yday_18z}/{f_18z}", stream=True)
-            y_12z = requests.get(f"{yday_12z}/{f_12z}", stream=True)
-            y_06z = requests.get(f"{yday_06z}/{f_06z}", stream=True)
-            y_00z = requests.get(f"{yday_00z}/{f_00z}", stream=True)    
-    
-        else:
-            t_18z = requests.get(f"{today_18z}/{f_18z}", stream=True, proxies=proxies)
-            t_12z = requests.get(f"{today_12z}/{f_12z}", stream=True, proxies=proxies)
-            t_06z = requests.get(f"{today_06z}/{f_06z}", stream=True, proxies=proxies)
-            t_00z = requests.get(f"{today_00z}/{f_00z}", stream=True, proxies=proxies)
-    
-            y_18z = requests.get(f"{yday_18z}/{f_18z}", stream=True, proxies=proxies)
-            y_12z = requests.get(f"{yday_12z}/{f_12z}", stream=True, proxies=proxies)
-            y_06z = requests.get(f"{yday_06z}/{f_06z}", stream=True, proxies=proxies)
-            y_00z = requests.get(f"{yday_00z}/{f_00z}", stream=True, proxies=proxies)         
-    
-        if t_18z.status_code == 200:
-            url = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{now.strftime('%Y%m%d')}/18/atmos/pgrb2{a}p{b}/"
-        elif t_18z.status_code != 200 and t_12z.status_code == 200:
-            url = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{now.strftime('%Y%m%d')}/12/atmos/pgrb2{a}p{b}/"
-        elif t_12z.status_code != 200 and t_06z.status_code == 200:
-            url = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{now.strftime('%Y%m%d')}/06/atmos/pgrb2{a}p{b}/"       
-        elif t_06z.status_code != 200 and t_00z.status_code == 200:
-            url = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{now.strftime('%Y%m%d')}/00/atmos/pgrb2{a}p{b}/"
-        elif t_00z.status_code != 200 and y_18z.status_code == 200:
-            url = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{yd.strftime('%Y%m%d')}/18/atmos/pgrb2{a}p{b}/"
-        elif y_18z.status_code != 200 and y_12z.status_code == 200:
-            url = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{yd.strftime('%Y%m%d')}/12/atmos/pgrb2{a}p{b}/"      
-        elif y_12z.status_code != 200 and y_06z.status_code == 200:
-            url = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{yd.strftime('%Y%m%d')}/06/atmos/pgrb2{a}p{b}/"
-        else:
-            url = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{yd.strftime('%Y%m%d')}/00/atmos/pgrb2{a}p{b}/"
+    1) western_bound (Float or Integer)
+    2) eastern_bound (Float or Integer)
 
-        url_run = int(f"{url[aa]}{url[bb]}")
-        
-    return url, url_run
+    Returns
+    -------
+
+    Western and Eastern Bounds with 0 to 360 coordinates. 
+    """
+    if western_bound < 0:
+        western_bound = abs(western_bound)
+    else:
+        western_bound = 360 - western_bound
+
+    if eastern_bound < 0:
+        eastern_bound = abs(eastern_bound)
+    else:
+        eastern_bound = 360 - eastern_bound
+
+    return western_bound, eastern_bound
+
+def cyclic_point(ds, parameter, lon_name='longitude'):
+
+    var = ds[parameter]
+    var_lon = var[lon_name]
+    var_lon_idx = var.dims.index(lon_name)
+    var, lon = add_cyclic_point(var.values, coord=var_lon, axis=var_lon_idx)
+
+    return var, lon
+
+def get_u_and_v(wind_speed, wind_dir):
+
+    """
+    This function calculates the u and u wind components
+
+    Required Arguments:
+
+    1) wind_speed (Float or Integer) 
+
+    2) wind_direction (Float or Integer)
+
+    Returns
+    -------
+
+    u and v wind components
+    """
+
+    u = wind_speed * np.cos(wind_dir)
+    v = wind_speed * np.sin(wind_dir)
+
+    return u, v
+
+def saturation_vapor_pressure(temperature):
+
+    """
+    This function calculates the saturation vapor pressure from temperature.
+    This function uses the formula from Bolton 1980.   
+
+    Required Arguments:
+
+    1) temperature (Float or Integer)
+
+    Returns
+    -------
+
+    The saturation vapor pressure
+    """
+
+    e = 6.112 * np.exp(17.67 * (temperature) / (temperature + 243.5))
+    return e
+
+
+def relative_humidity(temperature, dewpoint):
+
+    """
+    This function calculates the relative humidity from temperature and dewpoint. 
+
+    Required Arguments:
+
+    1) temperature (Float or Integer)
+
+    2) dewpoint (Float or Integer)
+
+    Returns
+    -------
+
+    The relative humidity
+    """
+
+    e = saturation_vapor_pressure(dewpoint)
+    e_s = saturation_vapor_pressure(temperature)
+    return (e / e_s) * 100
