354503321ed09b54f7cac2748c61457fe0bf7959
edrewitz <drewitzwx@gmail.com>
2025-08-05 18:11:20 -0700

    added fems back as the issue was fixed

--- wxdata/__init__.py
+++ wxdata/__init__.py
@@ -4,6 +4,12 @@ from wxdata.gfs.gefs import(
     gefs_0p25
 )
 
+from wxdata.fems.fems import(
+    get_single_station_data,
+    get_raws_sig_data,
+    get_nfdrs_forecast_data
+)
+
 from wxdata.noaa.nws import get_ndfd_grids
 from wxdata.soundings.wyoming_soundings import get_observed_sounding_data
 from wxdata.utils.utils import cyclic_point
